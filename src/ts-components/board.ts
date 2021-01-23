ee891ed07a9e801fbc5c9ff62e55afaa3d12c0e6
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-23 12:19:43 +0100

    fix board colors and notation

--- src/ts-components/board.ts
+++ src/ts-components/board.ts
@@ -8,14 +8,14 @@ class Board{
         const boardContainer = document.createElement('div');
         boardContainer.classList.add('board-container');
 
-        for(let i=1; i<9; i++){
+        for(let i=8; i>0; i--){
             for(let j=1; j<9; j++){
                 const div = document.createElement('div');
                 div.id = `${i}-${ID[j]}`; //<-- ważne żeby każdą komórkę na planszy dało się zidentyfikować za pomocą dwóch współrzędnych
                 if(i%2 === 0)
-                    div.classList.add(`${j%2 === 0 ? 'light' : 'dark'}`);
-                else
                     div.classList.add(`${j%2 === 0 ? 'dark' : 'light'}`);
+                else
+                    div.classList.add(`${j%2 === 0 ? 'light' : 'dark'}`);
                 boardContainer.appendChild(div);
             }
         }
