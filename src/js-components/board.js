cd22fd66538e8502ef842832d67df866bd23cbd6
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-23 18:12:04 +0100

    worling with styles

--- src/js-components/board.js
+++ src/js-components/board.js
@@ -14,8 +14,23 @@ var ID;
 })(ID || (ID = {}));
 class Board {
     drawBoard() {
+        const boardWrapper = document.createElement('div');
+        boardWrapper.classList.add('board-wrapper');
+        const letters = document.createElement('div');
+        letters.classList.add('letters');
+        const numbers = document.createElement('div');
+        numbers.classList.add('numbers');
         const boardContainer = document.createElement('div');
         boardContainer.classList.add('board-container');
+        for (let i = 1; i < 9; i++) {
+            const div = document.createElement('div');
+            const num = document.createElement('div');
+            div.classList.add('notation');
+            div.innerText = `${ID[i]}`;
+            num.innerText = `${i}`;
+            letters.appendChild(div);
+            numbers.appendChild(num);
+        }
         for (let i = 8; i > 0; i--) {
             for (let j = 1; j < 9; j++) {
                 const div = document.createElement('div');
@@ -27,7 +42,9 @@ class Board {
                 boardContainer.appendChild(div);
             }
         }
-        document.querySelector('body').appendChild(boardContainer);
+        document.querySelector('body').appendChild(boardWrapper).appendChild(numbers);
+        boardWrapper.appendChild(boardContainer);
+        boardWrapper.appendChild(letters);
     }
 }
 exports.Board = Board;
--- src/ts-components/board.ts
+++ src/ts-components/board.ts
@@ -5,8 +5,27 @@ enum ID {
 class Board{
 
     drawBoard(){
+        const boardWrapper = document.createElement('div');
+        boardWrapper.classList.add('board-wrapper');
+
+        const letters= document.createElement('div');
+        letters.classList.add('letters');
+
+        const numbers= document.createElement('div');
+        numbers.classList.add('numbers');
+
         const boardContainer = document.createElement('div');
         boardContainer.classList.add('board-container');
+
+        for(let i =1; i<9; i++){
+            const div = document.createElement('div');
+            const num = document.createElement('div');
+            div.classList.add('notation');
+            div.innerText = `${ID[i]}`;
+            num.innerText = `${i}`;
+            letters.appendChild(div);
+            numbers.appendChild(num);
+        }
 
         for(let i=8; i>0; i--){
             for(let j=1; j<9; j++){
@@ -20,7 +39,9 @@ class Board{
             }
         }
 
-        document.querySelector('body')!.appendChild(boardContainer);
+        document.querySelector('body')!.appendChild(boardWrapper).appendChild(numbers);
+        boardWrapper.appendChild(boardContainer);
+        boardWrapper.appendChild(letters);
     }
 }
 
