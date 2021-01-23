d154f2cb99709f3daf6abaae891772a634cc27f6
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-23 19:49:30 +0100

    board work

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import {King} from "./js-components/pieces/king";
 var gameBoard = new Board();
 gameBoard.drawBoard();
 
-var kingWhite = new King('white', 'B', 4);
-kingWhite.setOnBoard(0, 0);
+var kingWhite = new King('white', 'C', 4);
+kingWhite.setOnBoard('C', 1);
