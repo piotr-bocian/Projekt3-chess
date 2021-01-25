e90d6f14ab0c234cbfaeb46435a1e7fae83518a7
double_s <sz.suchodolski@gmail.com>
2021-01-25 23:21:54 +0100

    added function fillBoard which sets all pieces on a board

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import {Board} from "./js-components/board";
-import {King} from "./js-components/pieces/king";
-import {Queen} from "./js-components/pieces/queen";
-import {Bishop} from "./js-components/pieces/bishop";
-import {Knight} from "./js-components/pieces/knight";
-import {Rook} from "./js-components/pieces/rook";
-import {Pawn} from "./js-components/pieces/pawn";
+import {fillBoard} from "./js-components/fillBoard";
 
-var gameBoard = new Board();
+const gameBoard = new Board();
 gameBoard.drawBoard();
-
-var kingWhite = new King('white', 'E', 1);
-// kingWhite.setOnBoard('C', 1);
-
-var queenWhite = new Queen('white', 'D', 1);
-var bishopWhite = new Bishop('white', 'C', 1);
-var bishopWhite2 = new Bishop('white', 'F', 1);
-var knightWhite = new Knight('white', 'B', 1);
-var knightWhite2 = new Knight('white', 'G', 1);
-var rookWhite = new Rook('white', 'A', 1);
-var rookWhite2 = new Rook('white', 'H', 1);
-var pawnWhite = new Pawn('white', 'A', 2);
-var pawnWhite2 = new Pawn('white', 'B', 2);
-var pawnWhite3 = new Pawn('white', 'C', 2);
-var pawnWhite4 = new Pawn('white', 'D', 2);
-var pawnWhite5 = new Pawn('white', 'E', 2);
-var pawnWhite6 = new Pawn('white', 'F', 2);
-var pawnWhite7 = new Pawn('white', 'G', 2);
-var pawnWhite8 = new Pawn('white', 'H', 2);
+fillBoard();
---
+++ src/js-components/fillBoard.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fillBoard = void 0;
+const king_1 = require("./pieces/king");
+const queen_1 = require("./pieces/queen");
+const bishop_1 = require("./pieces/bishop");
+const knight_1 = require("./pieces/knight");
+const rook_1 = require("./pieces/rook");
+const pawn_1 = require("./pieces/pawn");
+const board_1 = require("./board");
+const fillBoard = () => {
+    //whites
+    const kingWhite = new king_1.King('white', `${board_1.ID[5]}`, 1);
+    const queenWhite = new queen_1.Queen('white', `${board_1.ID[4]}`, 1);
+    for (let i = 3; i <= 6; i += 3) {
+        const bishopWhite = new bishop_1.Bishop('white', `${board_1.ID[i]}`, 1);
+    }
+    for (let i = 2; i <= 7; i += 5) {
+        const knightWhite = new knight_1.Knight('white', `${board_1.ID[i]}`, 1);
+    }
+    for (let i = 1; i <= 8; i += 7) {
+        const rookWhite = new rook_1.Rook('white', `${board_1.ID[i]}`, 1);
+    }
+    for (let i = 1; i <= 8; i++) {
+        const pawnWhite = new pawn_1.Pawn('white', `${board_1.ID[i]}`, 2);
+    }
+    //blacks
+    const kingBlack = new king_1.King('white', `${board_1.ID[5]}`, 8);
+    const queenBlack = new queen_1.Queen('white', `${board_1.ID[4]}`, 8);
+    for (let i = 3; i <= 6; i += 3) {
+        const bishopBlack = new bishop_1.Bishop('white', `${board_1.ID[i]}`, 8);
+    }
+    for (let i = 2; i <= 7; i += 5) {
+        const knightBlack = new knight_1.Knight('white', `${board_1.ID[i]}`, 8);
+    }
+    for (let i = 1; i <= 8; i += 7) {
+        const rookBlack = new rook_1.Rook('white', `${board_1.ID[i]}`, 8);
+    }
+    for (let i = 1; i <= 8; i++) {
+        const pawnBlack = new pawn_1.Pawn('white', `${board_1.ID[i]}`, 7);
+    }
+};
+exports.fillBoard = fillBoard;
