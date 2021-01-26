8a90d609f5a793e801f547f221418cbd33268cf5
Jan Eliasz <johnbg@tlen.pl>
2021-01-26 17:58:59 +0100

    Create Game class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import {Board} from "./js-components/board";
 import {fillBoard} from "./js-components/fillBoard";
+import {Game} from "./js-components/game"
 
-const gameBoard = new Board();
-gameBoard.drawBoard();
-fillBoard();
+// const gameBoard = new Board();
+// gameBoard.drawBoard();
+// fillBoard();
+
+const game = new Game();
---
+++ src/ts-components/game.ts
@@ -0,0 +1,44 @@
+import { Board } from "./board";
+import { Bishop } from "./pieces/bishop";
+import { King } from "./pieces/king";
+import { Knight } from "./pieces/knight";
+import { Piece } from "./pieces/piece";
+import { Queen } from "./pieces/queen";
+import { Rook } from "./pieces/rook";
+import { Pawn } from "./pieces/pawn"
+import { ID } from "./board";
+
+class Game {
+
+    private gameBoard:Board;
+    
+    //private whoNext:string;
+
+    private whites:Piece[] = [];
+
+    constructor(){
+        this.gameBoard = new Board;
+        this.gameBoard.drawBoard();
+
+        this.whites.push(new Queen('white', `${ID[4]}`, 1));
+        this.whites.push(new King('white', `${ID[5]}`, 1));
+        
+        for(let i = 3; i <= 6; i+=3) {
+            this.whites.push(new Bishop('white', `${ID[i]}`, 1));
+        }
+
+        for(let i = 2; i <= 7; i+=5) {
+            this.whites.push(new Knight('white', `${ID[i]}`, 1));
+        }
+
+        for(let i = 1; i <= 8; i+=7) {
+            this.whites.push(new Rook('white', `${ID[i]}`, 1));
+        }
+
+        for(let i = 1; i <= 8; i++) {
+            this.whites.push(new Pawn('white', `${ID[i]}`, 2));
+        }
+    }
+}
+
+export {Game};
