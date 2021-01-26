2bbeeb1b8e2e06d4d7b968dcc06f8a9761b8b2ab
double_s <sz.suchodolski@gmail.com>
2021-01-26 19:42:07 +0100

    commented all pieces apart from knight

--- src/ts-components/game.ts
+++ src/ts-components/game.ts
@@ -20,24 +20,24 @@ class Game {
         this.gameBoard = new Board;
         this.gameBoard.drawBoard();
 
-        this.whites.push(new Queen('white', `${ID[4]}`, 1));
-        this.whites.push(new King('white', `${ID[5]}`, 1));
+        // this.whites.push(new Queen('white', `${ID[4]}`, 1));
+        // this.whites.push(new King('white', `${ID[5]}`, 1));
         
-        for(let i = 3; i <= 6; i+=3) {
-            this.whites.push(new Bishop('white', `${ID[i]}`, 1));
-        }
+        // for(let i = 3; i <= 6; i+=3) {
+        //     this.whites.push(new Bishop('white', `${ID[i]}`, 1));
+        // }
 
         for(let i = 2; i <= 7; i+=5) {
             this.whites.push(new Knight('white', `${ID[i]}`, 1));
         }
 
-        for(let i = 1; i <= 8; i+=7) {
-            this.whites.push(new Rook('white', `${ID[i]}`, 1));
-        }
+        // for(let i = 1; i <= 8; i+=7) {
+        //     this.whites.push(new Rook('white', `${ID[i]}`, 1));
+        // }
 
-        for(let i = 1; i <= 8; i++) {
-            this.whites.push(new Pawn('white', `${ID[i]}`, 2));
-        }
+        // for(let i = 1; i <= 8; i++) {
+        //     this.whites.push(new Pawn('white', `${ID[i]}`, 2));
+        // }
     }
 
     startMove(square:HTMLElement){
