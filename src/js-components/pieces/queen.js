745d470e5df636499c9816e30b886ff09820117a
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-26 21:16:56 +0100

    possible moves push in to arr

--- src/js-components/pieces/queen.js
+++ src/js-components/pieces/queen.js
@@ -4,13 +4,19 @@ exports.Queen = void 0;
 const piece_1 = require("./piece");
 //królowa / hetman
 class Queen extends piece_1.Piece {
-    constructor(color, positionX, positionY) {
+    constructor(color, positionX, positionY, possibleMoves) {
         super(color, positionX, positionY);
         this.symbol = `../../../../Projekt3-chess/static/assets/whiteQueen.png`;
         this.setOnBoard(this.positionX, this.positionY);
+        this.possibleMoves = this.showPossibleMoves();
     }
     showPossibleMoves() {
-        //kod odpowiadający za pokazanie możliwych ruchów
+        const moves = [];
+        for (let i = 1; i < 9; i++) {
+            if (`${this.positionX}-${i}` !== `${this.positionX}-${this.positionY}`)
+                moves.push(`${this.positionX}-${i}`);
+        }
+        console.log(moves);
     }
 }
 exports.Queen = Queen;
--- src/ts-components/pieces/queen.ts
+++ src/ts-components/pieces/queen.ts
@@ -1,16 +1,23 @@
+import { ID } from "../board";
 import {Piece} from "./piece";
 //królowa / hetman
 class Queen extends Piece{
-
-    constructor(color:string, positionX:string, positionY:number){
+protected possibleMoves:void;
+    constructor(color:string, positionX:string, positionY:number, possibleMoves:void ){
         super(color, positionX, positionY);
         this.symbol = `../../../../Projekt3-chess/static/assets/whiteQueen.png`;
-
         this.setOnBoard(this.positionX, this.positionY);
+        this.possibleMoves = this.showPossibleMoves()
     }
 
     showPossibleMoves():void{
-        //kod odpowiadający za pokazanie możliwych ruchów
+        const moves = [];
+        for(let i=1; i<9; i++){
+            if (`${this.positionX}-${i}` !== `${this.positionX}-${this.positionY}`)
+                moves.push(`${this.positionX}-${i}`)
+        }
+        console.log(moves);
+
     }
 }
 
