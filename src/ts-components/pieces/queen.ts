7478c286c4decfe7512c0d78910763bf036f2e67
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-31 16:48:08 +0100

    all work

--- src/ts-components/pieces/queen.ts
+++ src/ts-components/pieces/queen.ts
@@ -8,15 +8,13 @@ class Queen extends Piece implements QueenMethods{
         this.symbol = `../../../static/assets/${this.color}Queen.png`;
         this.symbol = `../../../../Projekt3-chess/static/assets/whiteQueen.png`;
         this.setOnBoard(this.positionX, this.positionY);
-        this.history
     }
 
     showPossibleMoves(){
         const movesShow:MovesShow =(id)=>{
             const movesPossibilities = [...document.querySelectorAll(`#${id}`)];
                 movesPossibilities.forEach(el=>{
-                    if (el.classList.contains('pieceInside')) return
-                    el.classList.toggle('queenMove');
+                    el.classList.toggle('active');
                 })
          }
          this.collectAllPossibleMoves().forEach(id=>{
@@ -29,74 +27,136 @@ class Queen extends Piece implements QueenMethods{
         const squares = [...document.querySelectorAll('.board-container div')];
         squares.forEach(square => {
             square.addEventListener('click', () => {
-                if ((square).classList.contains('pieceInside')) return;
-                const history:string[]=[];
-                const x = this.getPositionX();
-                const y = this.getPositionY();
-                const h = `from ${x}-${y} to ${(square).id.charAt(0)}-${parseInt((square).id.charAt(2))}`
-                history.push(h)
-                if(!(square).classList.contains('pieceInside') && (square).classList.contains('queenMove')){
+                if(!(square).classList.contains('pieceInside') && (square).classList.contains('active')){
                     this.setOnBoard((square).id.charAt(0), parseInt((square).id.charAt(2)));
                     this.removeClassActive()
                 }
             });
         });
     }
-    collectAllPossibleMoves(){
-        // console.log(parseInt(this.positionX, 36) - 9); tworzy liczbę z litery a=1,b=2 itd, używam do enuma ID
-        const moves:string[]=[];
 
-        const upDown=()=>{
-            for(let i=1; i<9; i++){
-            if (`${this.positionX}-${i}` !== `${this.positionX}-${this.positionY}`)
-            moves.push(`${this.positionX}-${i}`)
-        }}
+   collectAllPossibleMoves(){
+    const coordinateX : number = Object.values(ID).indexOf(this.positionX) + 1;
+    const moves:string[]=[];
 
-        const leftRight=()=>{
-            for(let i=1; i<9; i++){
-            if (`${ID[i]}-${this.positionY}` !== `${this.positionX}-${this.positionY}`)
-            moves.push(`${ID[i]}-${this.positionY}`)
-        }}
-
-        const diagonal=()=>{
-            const regexLetters = /[A-H]+/;
-            const regexNumbers = /[1-8]+/;
-            for(let i=1; i<=8; i++){
-                //x+1,y+1
-    if(`${ID[(parseInt(this.positionX, 36) - 9) + i]}-${this.positionY + i}`.match(regexNumbers) &&
-    `${ID[(parseInt(this.positionX, 36) - 9) + i]}-${this.positionY + i}`.match(regexLetters)){
-
-        moves.push(`${ID[(parseInt(this.positionX, 36) - 9) + i]}-${this.positionY + i}`);
+    const moveUp=()=>{
+    for(let i=this.positionY +1; i<9; i++){
+        const checkSquare = document.querySelector(`#${this.positionX}-${i}`);
+    if (checkSquare?.classList.contains('pieceInside') || checkSquare == null) return
+    moves.push(`${this.positionX}-${i}`)
+}
+}
+    const moveDown = ()=>{
+        for(let j=this.positionY - 1; j>0; j--){
+            const checkSquare = document.querySelector(`#${this.positionX}-${j}`);
+        if (checkSquare?.classList.contains('pieceInside') || checkSquare === null) return
+        moves.push(`${this.positionX}-${j}`)
+        }
     }
-                // x-1,y-1
-    if(`${ID[(parseInt(this.positionX, 36) - 9) - i]}-${this.positionY - i}`.match(regexNumbers) &&
-    `${ID[(parseInt(this.positionX, 36) - 9) - i]}-${this.positionY - i}`.match(regexLetters)){
+    const moveRight=()=>{
 
-        moves.push(`${ID[(parseInt(this.positionX, 36) - 9)-i]}-${this.positionY-i}`);
+        for(let i=coordinateX +1; i<9; i++){
+            const checkSquare = document.querySelector(`#${ID[i]}-${this.positionY}`);
+        if (checkSquare?.classList.contains('pieceInside') || checkSquare == null) return
+        moves.push(`${ID[i]}-${this.positionY}`)
     }
-                //x+1,y-1
-    if(`${ID[(parseInt(this.positionX, 36) - 9) + i]}-${this.positionY - i}`.match(regexNumbers) &&
-    `${ID[(parseInt(this.positionX, 36) - 9) + i]}-${this.positionY - i}`.match(regexLetters)){
-
-        moves.push(`${ID[(parseInt(this.positionX, 36) - 9) + i]}-${this.positionY - i}`);
     }
-                //x-1,y+1
-    if(`${ID[(parseInt(this.positionX, 36) - 9) - i]}-${this.positionY + i}`.match(regexNumbers) &&
-    `${ID[(parseInt(this.positionX, 36) - 9) - i]}-${this.positionY + i}`.match(regexLetters)){
-
-        moves.push(`${ID[(parseInt(this.positionX, 36) - 9) - i]}-${this.positionY + i}`);
-    }
+    const moveLeft=()=>{
+                for(let i=coordinateX -1; i>0; i--){
+                    const checkSquare = document.querySelector(`#${ID[i]}-${this.positionY}`);
+                if (checkSquare?.classList.contains('pieceInside') || checkSquare == null) return
+                moves.push(`${ID[i]}-${this.positionY}`)
+            }
+            }
+    const diagonalMoves=()=>{
+    if (this.color === 'white') {
+        // top right
+        if (9 - coordinateX < 9 - this.positionY) {
+            for(let i=1; i<9 - coordinateX; i++){
+                if (document.getElementById(`${ID[coordinateX+i]}-${this.positionY+i}`)!.classList.contains('pieceInside')) {
+                    break;
+                } else {
+                    moves.push(`${ID[coordinateX+i]}-${this.positionY+i}`);
+                }
+            }
+        } else {
+            for(let i=1; i < 9 - this.positionY; i++){
+                if (document.getElementById(`${ID[coordinateX+i]}-${this.positionY+i}`)!.classList.contains('pieceInside')) {
+                    break;
+                } else {
+                    moves.push(`${ID[coordinateX+i]}-${this.positionY+i}`);
+                }
             }
         }
-        diagonal()
-        upDown()
-        leftRight()
-        return moves;
-   }
+
+        // down left
+        if (this.positionY - 1 < coordinateX - 1) {
+            for(let i=1 ; i < this.positionY; i++){
+                if (document.getElementById(`${ID[coordinateX-i]}-${this.positionY-i}`)!.classList.contains('pieceInside')) {
+                    break;
+                } else {
+                    moves.push(`${ID[coordinateX - i]}-${this.positionY - i}`);
+                }
+            }
+        } else {
+            for(let i=1 ; i < coordinateX; i++){
+                if (document.getElementById(`${ID[coordinateX-i]}-${this.positionY-i}`)!.classList.contains('pieceInside')) {
+                    break;
+                } else {
+                    moves.push(`${ID[coordinateX - i]}-${this.positionY - i}`);
+                }
+            }
+        }
+
+        // top left
+        if (coordinateX < 9 - this.positionY) {
+            for(let i = 1; i < coordinateX; i++){
+                if (document.getElementById(`${ID[coordinateX-i]}-${this.positionY+i}`)!.classList.contains('pieceInside')) {
+                    break;
+                } else {
+                    moves.push(`${ID[coordinateX-i]}-${this.positionY+i}`);
+                }
+            }
+        } else {
+            for(let i = 1 ; i < 9 - this.positionY; i++) {
+                if (document.getElementById(`${ID[coordinateX-i]}-${this.positionY + i}`)!.classList.contains('pieceInside')) {
+                    break;
+                } else {
+                    moves.push(`${ID[coordinateX - i]}-${this.positionY + i}`);
+                }
+            }
+        }
+
+        // down right
+        if (this.positionY < 9 - coordinateX) {
+            for(let i = 1 ; i < this.positionY ; i++){
+                if (document.getElementById(`${ID[coordinateX+i]}-${this.positionY-i}`)!.classList.contains('pieceInside')) {
+                    break;
+                } else {
+                    moves.push(`${ID[coordinateX + i]}-${this.positionY - i}`);
+                }
+            }
+        } else {
+            for(let i=1; i < 9 - coordinateX; i++) {
+                if (document.getElementById(`${ID[coordinateX+i]}-${this.positionY-i}`)!.classList.contains('pieceInside')) {
+                    break;
+                } else {
+                    moves.push(`${ID[coordinateX + i]}-${this.positionY - i}`);
+                }
+            }
+        }
+    }}
+    diagonalMoves()
+    moveUp()
+    moveDown()
+    moveLeft()
+    moveRight()
+    return moves;
+}
     removeClassActive(){
-    let elems= [...document.querySelectorAll('.queenMove')];
+    let elems= [...document.querySelectorAll('.active')];
     for (let i = 0; i < elems.length; i++) {
-            elems[i]?.classList.remove('queenMove');
+            elems[i]?.classList.remove('active');
     }
 }
 }
