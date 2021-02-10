229ba5ac511d118de58edec3ea722a2b8fb10d78
Klaudia <74868763+kwojc13@users.noreply.github.com>
2021-02-10 20:25:36 +0100

    Delete bundle.js

--- dist/bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";var e=document.querySelector("#secret-button"),t=document.querySelector("#secret-paragraph"),c=!1;function n(){t.style.display=c?"block":"none"}e.addEventListener("click",(function(){c=!c,n(),e.textContent=c?"Hide the Secret":"Show the Secret"})),n()})();
