42ee81033e7ad530b0389a30ba04eb3910452506
double_s <sz.suchodolski@gmail.com>
2021-02-10 20:26:19 +0100

    remove import fillBoard.js from index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import {Board} from "./js-components/board";
-import {fillBoard} from "./js-components/fillBoard";
 import {Game} from "./js-components/game"
 
-// const gameBoard = new Board();
-// gameBoard.drawBoard();
-// fillBoard();
 
 const game = new Game();
 
