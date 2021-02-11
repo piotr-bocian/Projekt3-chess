d90f38977c4961837cf34c2b77381cbb9325cc1a
Your Name <you@example.com>
2021-02-11 18:47:39 +0100

    END MODAL

--- src/js-components/end-page/endPage.js
+++ src/js-components/end-page/endPage.js
@@ -4,31 +4,37 @@ function title() {
     const titleDiv = document.createElement("div");
     titleDiv.className = "menuTitle";
     const title = document.createElement("h1");
-    title.innerHTML = "WYNIK GRY";
+    title.innerHTML = "KONIEC GRY";
     titleDiv.appendChild(title);
     return title;
 }
+
 function gameResult() {
 
     const testObj = {
         user1: 'Kamil',
         user2: "Maciek",
-        winner: '',
+        winner: 'Maciek',
+        how:'czas'
     }
+
     const nameDiv = document.createElement("div");
     nameDiv.className = "menuNames";
     const title = document.createElement("h2");
     const title2 = document.createElement("h3");
     title.className = "title-result";
     title2.className = "title-result";
-    if(testObj.winner === ''){
-        console.log('remis')
-        title.innerHTML = `PAT`;
+    if(testObj.how === 'pat'){
+        title.innerHTML = `PAT (REMIS)`;
         title2.innerHTML = `Gracz ${testObj.user1} zremisował z graczem ${testObj.user2}`
     }
+    else if(testObj.how == 'czas'){
+        title2.innerHTML = `Koniec Czasu Przeciwnika`;
+        title.innerHTML = `Wygrał gracz ${testObj.winner}`;
+    }
     else{
-        title.innerHTML = `SZACH-MAT`;
-        title2.innerHTML = `WYGRAŁ GRACZ ${testObj.winner}`
+        title.innerHTML = `Wygrał gracz ${testObj.winner}`;
+        title2.innerHTML = `SZACH-MAT`;
     }
     const inputDiv = document.createElement("div");
     nameDiv.appendChild(title);
@@ -45,11 +51,12 @@ function startButton() {
     startButtonContainer.appendChild(startButtonText);
     return startButtonContainer;
 }
-function endPage() {
+
+function endPage(obj) {
     const menu = document.createElement("div");
     menu.className = "menuContainer";
     menu.appendChild(title());
-    menu.appendChild(gameResult());
+    menu.appendChild(gameResult(obj));
     menu.appendChild(startButton());
     return menu;
 }
